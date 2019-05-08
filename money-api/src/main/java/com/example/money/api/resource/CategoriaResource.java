c74f4250c95a67b1d0599db7f0eeb62ad8dd32a5
Unknown <rafaelcerq91@gmail.com>
2019-05-07 23:45:06 -0300

    Implementação de métodos GET e POST para categorias

---
+++ money-api/src/main/java/com/example/money/api/resource/CategoriaResource.java
@@ -0,0 +1,57 @@
+package com.example.money.api.resource;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.money.api.model.Categoria;
+import com.example.money.api.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+	//Autowired para utilizar os recursos do JPA direto na interf
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@GetMapping
+	public List<Categoria> listar() {
+		return categoriaRepository.findAll();
+	}
+	
+/*	retornar 204 - no Content quando não encontra informações no servidor
+	public ResponseEntity<?> listar() {
+		List<Categoria> categorias = categoriaRepository.findAll();
+		//Se a lista n]ao estiver vazia responde 200, senão retorna 204
+		return !categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.noContent().build();
+	}
+*/
+	
+	@PostMapping
+	//@ResponseStatus(HttpStatus.CREATED)//como já tem o response no return, nao precisa desta notação
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public Categoria buscarPeloCodigo(@PathVariable Long codigo) {
+		return categoriaRepository.findById(codigo).get();
+	}
+}
